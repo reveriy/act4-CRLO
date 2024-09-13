eeda9b21209a1bd94ebdf61ddf6cc10ca4cdda11
Valorie Portem <valorieportem@gmail.com>
2024-09-13 20:44:37 +0800

    initiatl commit

---
+++ comparison.js
@@ -0,0 +1,70 @@
+let firstFavNumb = 21;
+let secondFavNumb = 14;
+
+console.log("USING GREATER THAN");
+let V = 21 > 14;
+console.log("My favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("21 > 14" );
+console.log(firstFavNumb > secondFavNumb);
+
+console.log("USING LESS THAN");
+let A = 21 < 14;
+console.log("My favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("21 < 14" );
+console.log(firstFavNumb < secondFavNumb);
+
+console.log("USING GREATER THAN OR EQUAL TO");
+let L = 21 >= 14;
+console.log("My favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("21 >= 14" );
+console.log(firstFavNumb >= secondFavNumb);
+
+console.log("USING LESS THAN OR EQUAL TO");
+let O = 21 <= 14;
+console.log("My favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("21 <= 14" );
+console.log(firstFavNumb <= secondFavNumb);
+
+console.log("USING STRICTLY EQUALITY");
+let R = 21 === 14;
+console.log("My favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("21 === 14" );
+console.log(firstFavNumb === secondFavNumb);
+
+console.log("USING LOOSE EQUALITY");
+let I = 21 == 14;
+console.log("My favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("21 == 14" );
+console.log(firstFavNumb == secondFavNumb);
+
+console.log("USING STRICT INEQUALITY");
+let E = 21 !== 14;
+console.log("My favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("21 !== 14" );
+console.log(firstFavNumb !== secondFavNumb);
+
+console.log("USING LOOSE INEQUALITY");
+let M = 21 != 14;
+console.log("My favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("21 != 14" );
+console.log(firstFavNumb != secondFavNumb);
+
+console.log("USING AND");
+console.log("My favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("21 && 14" );
+console.log(firstFavNumb>14 && 21>secondFavNumb);
+
+console.log("USING OR");
+console.log("My favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("21 != 14" );
+console.log(firstFavNumb != secondFavNumb);
